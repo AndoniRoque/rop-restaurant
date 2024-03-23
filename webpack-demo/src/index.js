a70e44348f79791afec091f0163890af26479eec
Andoni Roque <roque.andoni@gmail.com>
2024-03-23 12:55:56 -0300

    finished

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,37 +1,14 @@
 import _ from 'lodash';
-import './style.css'; 
-import Background from './background.jpg';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title);
-console.log(toml.owner.name);
-
-console.log(yaml.title);
-console.log(yaml.owner.name);
-
-console.log(json.title);
-console.log(json.owner.name);
-
-import myName from './myName';
-import {functionOne, functionTwo} from "./myModule";
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
-    element.textContent="Home";
-    element.classList.add('hello');
+    btn.innerHTML = 'Click me and check the concosle';
+    btn.onclick = printMe;
 
-    const background = new Image();
-    background.src = Background;
-
-    element.appendChild(background);
-
-    console.log(Data);
-    console.log(Notes);
+    element.appendChild(btn);
 
     return element;
 }
--- webpack-demo/src/myName.js
+++
@@ -1,3 +0,0 @@
-const myName = (name) => "Hi! My name is chky chky " + name;
-
-export default myName;
